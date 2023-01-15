5153052b6065fb1e4e180f22ad5d0bbb2ce3805d
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2023-01-15 13:28:19 +0900

    board finish

--- polls/views.py
+++ polls/views.py
@@ -3,8 +3,35 @@ from django.shortcuts import render
 # Create your views here.
 
 from polls.models import Question, Answer
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 def index(request):
     question_list = Question.objects.all()
     context = {'latest_question_list': question_list}
     return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    question = Question.objects.get(id=question_id)
+    context = {'question': question}
+    return render(request, 'polls/detail.html', context)
+
+def vote(request, question_id):
+    question = Question.objects.get(id=question_id)
+    try:
+        selected_answer = question.answer_set.get(id=request.POST['answer'])
+    except (KeyError, Answer.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_answer.votes += 1
+        selected_answer.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+def results(request, question_id):
+    question = Question.objects.get(id=question_id)
+    context = {'question': question}
+    return render(request, 'polls/results.html', context)
+    
