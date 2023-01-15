c253b612a4a2072abe5a9271235b41774a49787b
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2023-01-15 13:22:16 +0900

    board

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from polls.models import Question, Answer
+
+def index(request):
+    question_list = Question.objects.all()
+    context = {'latest_question_list': question_list}
+    return render(request, 'polls/index.html', context)
